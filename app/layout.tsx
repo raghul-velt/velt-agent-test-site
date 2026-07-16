7d127bae216951cdcfe1e5862a7efa2e981a8911
Raghul <raghul@velt.dev>
2026-07-16 22:21:55 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,12 +32,12 @@ export default function RootLayout({
         data-sf-platform="other-manual"
         async
       />     */}
- <Script
+ {/* <Script
         id="superflowToolbarScript"
         src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=jyZk8hn7vUz7BkRwFPrH&projectId=3141301152185640"
         data-sf-platform="other-manual"
         async
-      />       
+      />        */}
       <body>{children}</body>
     </html>
   );
