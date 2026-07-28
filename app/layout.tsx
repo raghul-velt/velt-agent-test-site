6c44f281cf86c3aca4bedfe54a95dfe4105e56b5
Raghul <raghul@velt.dev>
2026-07-28 18:27:33 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,12 +26,12 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
 
-{/* <Script
+<Script
         id="superflowToolbarScript"
-        src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=eQgbu7SRboUpbAv8u5st&projectId=3141301152185640"
+        src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=rNYzMJd13L27xtfipmSK&projectId=3141301152185640"
         data-sf-platform="other-manual"
         async
-      />       */}
+      />      
       <body>{children}</body>
     </html>
   );
